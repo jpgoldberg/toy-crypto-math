1120b8e584803b5248a50a8400ca664fd6410693
Jeffrey Goldberg <jeff@agilebits.com>
2024-04-18 22:20:10 -0500

    (e)gcd tests

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -1,11 +1,11 @@
 import pytest
 import sys
-from math_utils import FactorList, factor, OLF, miller_rabin
+from math_utils import FactorList, factor, OLF, miller_rabin, gcd, egcd
+
 
 class TestFactor:
-
     def test_factor_small(self) -> None:
-        test_vectors: list[tuple[int, list[tuple[int, int]]]] = [
+        vectors: list[tuple[int, list[tuple[int, int]]]] = [
             (2, [(2, 1)]),
             (10, [(2, 1), (5, 1)]),
             (9, [(3, 2)]),
@@ -14,19 +14,22 @@ class TestFactor:
             (3557, [(3557, 1)]),
             (1999162, [(2, 1), (11, 3), (751, 1)]),
             ((2**32) - 1, [(3, 1), (5, 1), (17, 1), (257, 1), (65537, 1)]),
-            (2**64 - 1, [(3, 1), (5, 1), (17, 1), (257, 1), (641, 1), (65537, 1), (6700417, 1)]),
+            (
+                2**64 - 1,
+                [(3, 1), (5, 1), (17, 1), (257, 1), (641, 1), (65537, 1), (6700417, 1)],
+            ),
         ]
 
-        for n, expected in test_vectors:
+        for n, expected in vectors:
             assert factor(n) == expected
 
     def test_OLF(self) -> None:
-        test_vectors: list[tuple[int, int]] = [
+        vectors: list[tuple[int, int]] = [
             (22171, 1),
             (22171 * 45827 * 5483, 22171 * 5483),
         ]
 
-        for n, expected in test_vectors:
+        for n, expected in vectors:
             assert OLF(n) == expected
 
     def test_normalize(self) -> None:
@@ -51,100 +54,118 @@ class TestFactor:
     def test_factor_large(self) -> None:
         """for input that should trigger Fermat's method,"""
 
-        test_vectors: list[tuple[int, list[tuple[int, int]]]] = [
+        vectors: list[tuple[int, list[tuple[int, int]]]] = [
             (5483 * 5483, [(5483, 2)]),
             (5483 * 104243 * 11 * 11, [(11, 2), (5483, 1), (104243, 1)]),
             (2**32, [(2, 32)]),
             ((2**31) - 1, [(2147483647, 1)]),
         ]
 
-        for n, expected in test_vectors:
+        for n, expected in vectors:
             assert factor(n) == expected
 
     def test_phi(self) -> None:
-        test_vectors = [
+        vectors = [
             ([(3, 1), (5, 1)], 8),
             ([(2, 2), (5, 1)], 8),
             ([(65537, 1)], 65536),
             ([(2, 1), (5, 1), (3, 1)], 8),
         ]
-        for f, expected in test_vectors:
+        for f, expected in vectors:
             assert FactorList(f).phi == expected
 
     def test_n(self) -> None:
-        test_vectors = [
+        vectors = [
             ([(3, 1), (5, 1)], 15),
             ([(2, 2), (5, 1)], 20),
             ([(65537, 1)], 65537),
         ]
-        for input, expected in test_vectors:
+        for input, expected in vectors:
             f = FactorList(input)
             assert f.n == expected
             assert f.value() == expected
 
     def test_radical(self) -> None:
-        test_vectors = [
-            (
-                [(3, 1), (5, 1)],
-                [(3, 1), (5, 1)],
-                15
-            ),
-            (
-                [(2, 2), (5, 4)],
-                [(2, 1), (5, 1)],
-                10
-            ),
-            (
-                [(2, 32)],
-                [(2, 1)],
-                2
-            ),
+        vectors = [
+            ([(3, 1), (5, 1)], [(3, 1), (5, 1)], 15),
+            ([(2, 2), (5, 4)], [(2, 1), (5, 1)], 10),
+            ([(2, 32)], [(2, 1)], 2),
         ]
 
-        for input, exp_r, exp_rv in test_vectors:
+        for input, exp_r, exp_rv in vectors:
             f = FactorList(input)
             assert f.radical() == exp_r
             assert f.radical_value() == exp_rv
 
     def test_repr(self) -> None:
-        test_vectors = [
-            ([(5483, 2)], '5483^2'),
-            ([(11, 2), (5483, 1), (104243, 1)], '11^2 * 5483 * 104243'),
-            ([(2, 32)], '2^32'),
-            ([(2, 6), (3, 3), (5, 3)], '2^6 * 3^3 * 5^3'),
+        vectors = [
+            ([(5483, 2)], "5483^2"),
+            ([(11, 2), (5483, 1), (104243, 1)], "11^2 * 5483 * 104243"),
+            ([(2, 32)], "2^32"),
+            ([(2, 6), (3, 3), (5, 3)], "2^6 * 3^3 * 5^3"),
         ]
 
-        for input, expected in test_vectors:
+        for input, expected in vectors:
             f = FactorList(input)
             assert f.__repr__() == expected
 
     def test_coprimes(self) -> None:
-        test_vectors = [
+        vectors = [
             (15, [1, 2, 4, 7, 8, 11, 13, 14]),
             (17, list(range(1, 17))),
             (30, [1, 7, 11, 13, 17, 19, 23, 29]),
             (2**5, list(range(1, 2**5, 2))),  # all odd numbers < 2^5
         ]
 
-        for n, relprimes in test_vectors:
+        for n, relprimes in vectors:
             f = factor(n)
             assert list(f.coprimes()) == relprimes
 
 
 class TestMath:
-
     def test_mr(self) -> None:
-        test_vectors = [
+        vectors = [
             (512461, False),  # a Carmichael number
             (104297, True),
             (1180775137873020977354442912336269, True),
-            (1025046047436407593990706183629376939000352221561805965005888683119,
-            False),
+            (
+                1025046047436407593990706183629376939000352221561805965005888683119,
+                False,
+            ),
         ]
 
-        for n, is_prime in test_vectors:
+        for n, is_prime in vectors:
             assert miller_rabin(n) == is_prime
 
+    def test_gcd(self) -> None:
+        vectors = [
+            ((5, 50), 5),
+            ((18, 12), 6),
+        ]
 
-if __name__ == '__main__':
+        for input, expected in vectors:
+            a, b = input
+            assert gcd(a, b) == expected
+
+    def test_egcd(self) -> None:
+        vectors = [
+            (90, 409),
+            (464, 403),
+            (308, 463),
+            (384, 320),
+            (212, 351),
+            (382, 472),
+            (399, 107),
+            (469, 269),
+            (44, 163),
+            (464, 194),
+        ]
+
+        for a, b in vectors:
+            g, x, y = egcd(a, b)
+            assert g == gcd(a, b)
+            assert a * x + b * y == g
+
+
+if __name__ == "__main__":
     sys.exit(pytest.main())

e5e9c79e10da4cc5bb0b472fc779393fe02ed838
Jeffrey Goldberg <jeffrey@goldmark.org>
2024-10-09 16:14:20 -0500

    CSPRNG shuffle

--- src/toy_crypto/utils.py
+++ src/toy_crypto/utils.py
@@ -1,5 +1,6 @@
-from collections.abc import Generator
+from collections.abc import Generator, MutableSequence
 import math
+import secrets
 
 
 def lsb_to_msb(n: int) -> Generator[int, None, None]:
@@ -17,8 +18,22 @@ def lsb_to_msb(n: int) -> Generator[int, None, None]:
 
 
 def digit_count(x: float, b: int = 10) -> int:
-    """returns the nunmber of digits (base b) in the integer part of x"""
+    """returns the number of digits (base b) in the integer part of x"""
 
     x = abs(x)
     result = math.floor(math.log(x, base=b) + 1)
     return result
+
+
+def suffle(x: MutableSequence) -> MutableSequence:
+    "Shuffles sequence in places using a Cryptographically Secure RNG."
+
+    # Fisher-Yates Shuffle.
+    # https://en.wikipedia.org/wiki/Fisher–Yates_shuffle
+
+    n = len(x)
+    for i in range(n - 2):
+        j = secrets.randbelow(n - i) + i
+        x[i], x[j] = x[j], x[i]
+
+    return x

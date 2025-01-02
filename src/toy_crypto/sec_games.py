a6f0c15a8e959c867d1f19ed423664877a7d11b9
Jeffrey Goldberg <jeffrey@goldmark.org>
2025-01-02 00:18:12 -0600

    IndEav is subclass of Ind

--- src/toy_crypto/sec_games.py
+++ src/toy_crypto/sec_games.py
@@ -6,87 +6,13 @@ K = TypeVar("K")
 """Unbounded type variable intended for any type of key."""
 
 
-class IndEav(Generic[K]):
-    def __init__(
-        self,
-        key_gen: Callable[[], K],
-        encryptor: Callable[[K, bytes], bytes],
-    ) -> None:
-        """
-        IND-EAV game for symmetric encryption.
-
-        Indistinguishability in presence of an eavesdropper.
-
-        Takes an encryptor, which is a function that takes a key and bytes
-        and outputs bytes. And takes a
-        key generation function which
-        produces a randomly chosen key appropriate for the encryptor.
-
-        :param key_gen:
-            A function that generates a random key for the encryption scheme.
-        :param encryptor:
-            A function that takes a key and bytes and outputs bytes.
-        """
-
-        self._key_gen = key_gen
-        self._encryptor = encryptor
-        self._recent_b: Optional[bool]
-
-        self._key: Optional[K] = None
-        self._b: Optional[bool] = None
-
-    def initialize(self) -> None:
-        """Challenger picks key and a b."""
-        self._key = self._key_gen()
-        self._b = secrets.choice([True, False])
-
-    def encrypt_one(self, m0: bytes, m1: bytes) -> bytes:
-        """Challenger encrypts m0 if b is False, else encrypts m1.
-
-        :raise ValueError: if lengths of m0 and m1 are not equal.
-        :raises Exception: if challenge isn't initialized.
-        """
-
-        if self._key is None:
-            raise Exception("game is not in a state where this can be called")
-
-        if len(m0) != len(m1):
-            raise ValueError("Message lengths must be equal")
-
-        if self._b is None or self._key is None:
-            raise Exception("Challenge is not be properly initialized")
-
-        m = m1 if self._b else m0
-
-        encrypted = self._encryptor(self._key, m)
-
-        # prevent this from being called again with same key, b
-        self._key = None
-
-        return encrypted
-
-    def finalize(self, guess: bool) -> bool:
-        """
-        True iff guess is the same as b of previously created challenger.
-
-        Also resets the challenger, as for this game you cannot call with
-        same key, b pair more than once.
-        """
-
-        adv_wins = guess == self._b
-
-        self._b = None
-        self._key = None
-
-        return adv_wins
-
-
 _STATE_STARTED = "started"
 _STATE_INITIALIZED = "initialized"
 _STATE_ENCRYPTED = "encrypted_one"
 _STATE_FINALIZED = "finalized"
 
 # Next Allowed
+_NA_START = "start"
 _NA_INITIALIZE = "initialize"
 _NA_ENCRYPT_ONE = "encrypt_one"
 _NA_FINALIZE = "finalize"
@@ -163,7 +89,7 @@ class Ind(Generic[K]):
             raise Exception(f"{whoami} not allowed in state {self._state}")
         adv_wins = guess == self._b
 
-        self._state = _STATE_INITIALIZED
+        self._state = _STATE_STARTED
 
         return adv_wins
 
@@ -174,10 +100,44 @@ class IndCpa(Ind[K]):
         key_gen: Callable[[], K],
         encryptor: Callable[[K, bytes], bytes],
     ) -> None:
+        """IND-CPA game.
+
+        :param key_gen: A key generation function appropriate for encryptor
+        :param encryptor: A function that that takes a key and message and outputs ctext
+
+        The methods `initialize()`, `encrypt_one()`, and `finalize()` must
+        be called in a sequence appropriate for the specific game.
+        """
+
         super().__init__(key_gen=key_gen, encryptor=encryptor)
         self._state_map = {
             _STATE_STARTED: [_NA_INITIALIZE],
             _STATE_INITIALIZED: [_NA_ENCRYPT_ONE],
             _STATE_ENCRYPTED: [_NA_ENCRYPT_ONE, _NA_FINALIZE],
-            _STATE_FINALIZED: [_NA_INITIALIZE],
+            _STATE_FINALIZED: [_NA_START],
+        }
+
+
+class IndEav(Ind[K]):
+    def __init__(
+        self,
+        key_gen: Callable[[], K],
+        encryptor: Callable[[K, bytes], bytes],
+    ) -> None:
+        """IND-EAV game.
+
+        :param key_gen: A key generation function appropriate for encryptor
+        :param encryptor:
+            A function that that takes a key and message and outputs ctext
+
+        The methods `initialize()`, `encrypt_one()`, and `finalize()` must
+        be called in a sequence appropriate for the specific game.
+        """
+
+        super().__init__(key_gen=key_gen, encryptor=encryptor)
+        self._state_map = {
+            _STATE_STARTED: [_NA_INITIALIZE],
+            _STATE_INITIALIZED: [_NA_ENCRYPT_ONE],
+            _STATE_ENCRYPTED: [_NA_FINALIZE],
+            _STATE_FINALIZED: [_NA_START],
         }
--- tests/test_sec_games.py
+++ tests/test_sec_games.py
@@ -24,12 +24,14 @@ class TestInd:
         m0 = b"AA"
         m1 = b"AB"
 
-        challenger.initialize()
-        ctext = challenger.encrypt_one(m0, m1)
+        # chance of false pass is 2^trials
+        trials = 20
+        for _ in range(trials):
+            challenger.initialize()
+            ctext = challenger.encrypt_one(m0, m1)
+            guess: bool = ctext[0] != ctext[1]
 
-        guess: bool = ctext[0] != ctext[1]
-
-        assert challenger.finalize(guess)
+            assert challenger.finalize(guess)
 
     def test_eav_once(self) -> None:
         challenger = IndEav(self.key_gen, self.encryptor)
@@ -50,14 +52,15 @@ class TestInd:
         m0 = b"Attack at dawn!"
         m1 = b"Attack at dusk!"
 
-        challenger.initialize()
+        # chance of false pass is 2^trials
+        trials = 20
+        for _ in range(trials):
+            challenger.initialize()
+            ctext1 = challenger.encrypt_one(m0, m1)
+            ctext2 = challenger.encrypt_one(m1, m1)
+            guess = ctext1 == ctext2
 
-        ctext1 = challenger.encrypt_one(m0, m1)
-        ctext2 = challenger.encrypt_one(m1, m1)
-
-        guess = ctext1 == ctext2
-
-        assert challenger.finalize(guess)
+            assert challenger.finalize(guess)
 
 
 if __name__ == "__main__":

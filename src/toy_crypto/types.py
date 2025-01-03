a5dc16772ff10b15b6cdbce05473c6e3a90aa7b9
Jeffrey Goldberg <jeffrey@goldmark.org>
2025-01-03 17:45:55 -0600

    Bits

--- src/toy_crypto/types.py
+++ src/toy_crypto/types.py
@@ -6,7 +6,7 @@ They are not carefully thought out.
 This module is probably the least stable of any of these unstable modules.
 """
 
-from typing import Any, NewType, TypeGuard
+from typing import Any, NewType, Optional, TypeGuard, Protocol
 
 Prob = NewType("Prob", float)
 """Probability: A float between 0.0 and 1.0"""
@@ -44,3 +44,42 @@ def is_byte(val: Any) -> bool:
     if not isinstance(val, int):
         return False
     return 0 <= val and val < 256
+
+
+class SupportsBool(Protocol):
+    def __bool__(self) -> bool: ...
+
+
+class Bit:
+    """
+    Because I made poor choices earlier of how to represent
+    bits, I need an abstraction.
+    """
+
+    def __init__(self, b: SupportsBool) -> None:
+        self._value: bool = b is True
+        self._as_int: Optional[int] = None
+        self._as_bytes: Optional[bytes] = None
+
+    def as_bool(self) -> bool:
+        return self._value
+
+    def as_int(self) -> int:
+        if not self._as_int:
+            self._as_int = 1 if self._value else 0
+        return self._as_int
+
+    def as_bytes(self) -> bytes:
+        if not self._as_bytes:
+            self._as_bytes = (
+                (1).to_bytes(1, "big")
+                if self._value
+                else (0).to_bytes(1, "big")
+            )
+        return self._as_bytes
+
+    def __eq__(self, other: Any) -> bool:
+        try:
+            return self._value == other.__bool__()  # type: ignore[no-any-return]
+        except AttributeError:
+            return NotImplemented

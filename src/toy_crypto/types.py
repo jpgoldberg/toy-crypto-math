a427a1e8c7e5aae975b45359000d18d1b32cf967
Jeffrey Goldberg <jeffrey@goldmark.org>
2025-01-05 18:59:28 -0600

    Bit operations

--- src/toy_crypto/types.py
+++ src/toy_crypto/types.py
@@ -6,7 +6,18 @@ They are not carefully thought out.
 This module is probably the least stable of any of these unstable modules.
 """
 
-from typing import Any, NewType, Optional, TypeGuard, Protocol
+from typing import (
+    Any,
+    Callable,
+    NewType,
+    Optional,
+    TypeGuard,
+    Protocol,
+    Union,
+    runtime_checkable,
+)
+
+import operator
 
 Prob = NewType("Prob", float)
 """Probability: A float between 0.0 and 1.0"""
@@ -46,6 +57,7 @@ def is_byte(val: Any) -> bool:
     return 0 <= val and val < 256
 
 
+@runtime_checkable
 class SupportsBool(Protocol):
     def __bool__(self) -> bool: ...
 
@@ -60,6 +72,9 @@ class Bit:
         self._value: bool = b is True
         self._as_int: Optional[int] = None
         self._as_bytes: Optional[bytes] = None
+
+    def __bool__(self) -> bool:
+        return self._value
 
     def as_bool(self) -> bool:
         return self._value
@@ -79,7 +94,69 @@ class Bit:
         return self._as_bytes
 
     def __eq__(self, other: Any) -> bool:
-        try:
-            return self._value == other.__bool__()  # type: ignore[no-any-return]
-        except AttributeError:
+        if not isinstance(other, SupportsBool):
             return NotImplemented
+        ob = other.__bool__()
+
+        return self._value == ob
+
+    @staticmethod
+    def _other_bool(other: Any) -> Optional[bool]:
+        if isinstance(other, bytes):
+            ob = all([b != 0 for b in other])
+        elif not isinstance(other, SupportsBool):
+            return None
+        else:
+            ob = other.__bool__()
+        return ob
+
+    def _logic(
+        self, other: bool, expr: Callable[[bool, bool], bool]
+    ) -> Union["Bit", int, bool, bytes]:
+        """
+        Abstraction to manage type of :data:`other`
+        for things like :func:`__and__` and :func:`__or__`.
+        """
+        ob = self._other_bool(other)
+        if ob is None:
+            return NotImplemented
+        sb = self.as_bool()
+        tvalue = expr(sb, ob)
+
+        if isinstance(other, Bit):
+            return Bit(tvalue)
+
+        if isinstance(other, int):
+            return 1 if tvalue else 0
+        if isinstance(other, bytes):
+            return (1).to_bytes(1, "big") if tvalue else (0).to_bytes(1, "big")
+
+        return tvalue
+
+    def __and__(self, other: Any) -> Union["Bit", int, bool, bytes]:
+        ob = self._other_bool(other)
+        if ob is None:
+            return NotImplemented
+
+        return self._logic(other=ob, expr=lambda s, o: s and o)
+
+    def __xor__(self, other: Any) -> Union["Bit", int, bool, bytes]:
+        ob = self._other_bool(other)
+        if ob is None:
+            return NotImplemented
+
+        return self._logic(other=ob, expr=lambda s, o: operator.xor(s, o))
+
+    def __or__(self, other: Any) -> Union["Bit", int, bool, bytes]:
+        ob = self._other_bool(other)
+        if ob is None:
+            return NotImplemented
+
+        return self._logic(other=ob, expr=lambda s, o: s or o)
+
+    def inv(self) -> "Bit":
+        inv_b = not self.as_bool()
+        return Bit(inv_b)
+
+    def __inv__(self) -> "Bit":
+        return self.inv()

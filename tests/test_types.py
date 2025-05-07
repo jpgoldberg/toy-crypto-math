6a09bb5fcf16a739394369aa5c67b8edd13e23be
Jeffrey Goldberg <jeffrey@goldmark.org>
2025-05-06 22:24:31 -0500

    more test data for py ba

--- tests/test_types.py
+++ tests/test_types.py
@@ -4,12 +4,23 @@ import pytest
 from toy_crypto import types
 
 
-primes100 = [
-    2, 3, 5, 7, 11, 13, 17, 19,
-    23, 29, 31, 37, 41, 43, 47,
-    53, 59, 61, 67, 71, 73,
-    79, 83, 89, 97,
-]  # fmt: skip
+@pytest.fixture
+def sets() -> dict[str, set[int]]:
+    sets: dict[str, set[int]] = dict()
+    sets["universe"] = set(range(100))
+
+    sets["primes"] = {
+        2, 3, 5, 7, 11, 13, 17, 19,
+        23, 29, 31, 37, 41, 43, 47,
+        53, 59, 61, 67, 71, 73,
+        79, 83, 89, 97,
+    }  # fmt: skip
+
+    sets.update(
+        {f"multiples_{n}": set(range(0, 100, n)) for n in range(2, 12)}
+    )
+
+    return sets
 
 
 class TestPyBitArray:
@@ -27,29 +38,37 @@ class TestPyBitArray:
         for idx in range(length):
             assert ba[idx] == 1
 
-    def test_set(self) -> None:
-        ba = types.PyBitArray(100, fill_bit=0)
-        for p in primes100:
-            ba[p] = 1
+    def test_set(self, sets: dict[str, set[int]]) -> None:
+        universe = sets["universe"]
+        size = len(universe)
 
-        for i in range(100):
-            if i in primes100:
-                assert ba[i] == 1
-            else:
-                assert ba[i] == 0
+        for name, test_set in sets.items():
+            ba = types.PyBitArray(size, fill_bit=0)
+            for p in test_set:
+                ba[p] = 1
 
-    def test_unset(self) -> None:
-        ba = types.PyBitArray(100, fill_bit=1)
+            for i in universe:
+                if i in test_set:
+                    assert ba[i] == 1, f"set: {name}; i: {i}"
+                else:
+                    assert ba[i] == 0, f"set: {name}; i: {i}"
 
-        composites = set(range(100)) - set(primes100)
-        for c in composites:
-            ba[c] = 0
+    def test_unset(self, sets: dict[str, set[int]]) -> None:
+        universe = sets["universe"]
+        size = len(universe)
 
-        for i in range(100):
-            if i in primes100:
-                assert ba[i] == 1
-            else:
-                assert ba[i] == 0
+        for name, test_set in sets.items():
+            ba = types.PyBitArray(size, fill_bit=1)
+
+            complement = universe - test_set
+            for c in complement:
+                ba[c] = 0
+
+            for i in universe:
+                if i in test_set:
+                    assert ba[i] == 1, f"set: {name}; i: {i}"
+                else:
+                    assert ba[i] == 0, f"set: {name}; i: {i}"
 
 
 if __name__ == "__main__":

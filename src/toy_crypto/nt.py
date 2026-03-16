a8983b2ee7e5bd4f4d2ca72877e5724590b4042d
Jeffrey Goldberg <jeffrey@goldmark.org>
2026-03-16 13:46:59 -0500

    more (untested) crt

--- src/toy_crypto/nt.py
+++ src/toy_crypto/nt.py
@@ -564,7 +564,7 @@ def get_prime(
     return n
 
 
-class Crt:
+class CrtField:
     """
     Chinese Remainder Theorem
 
@@ -578,8 +578,19 @@ class Crt:
         partial_product: int
         inverse: int
 
+        def __eq__(self, other: object) -> bool:
+            if not isinstance(other, CrtField._Triple):
+                return NotImplemented
+            return (
+                self.modulus == other.modulus
+                and self.partial_product == other.partial_product
+            )
+
+        def __hash__(self) -> int:
+            return hash((self.modulus, self.partial_product))
+
     def __init__(self, moduli: Collection[types.PositiveInt]) -> None:
-        """Creates a CRT "world" with respect to moduli.
+        """Creates a CRT field with respect to moduli.
 
         .. warning::
 
@@ -592,25 +603,100 @@ class Crt:
             raise ValueError("At least one modulus must be given")
 
         # sort and eliminate duplicates
-        _moduli: Sequence[int] = sorted(list(set(list(moduli))))
-        if len(_moduli) != len(moduli):
+        self._moduli: Sequence[int] = sorted(list(set(list(moduli))))
+        if len(self._moduli) != len(moduli):
             logging.warning(
                 f"{self.__class__.__name__} initialization has duplicate moduli"
             )
 
-        for a, b in itertools.permutations(_moduli, 2):
+        for a, b in itertools.permutations(self._moduli, 2):
             if gcd(a, b) != 1:
                 # TODO: handle this properly. But for now just warn
                 logging.warning(f"moduli {a} and {b} are not coprime")
 
-        self._product = math.prod(_moduli)
+        self._product = math.prod(self._moduli)
 
-        partial_products: Sequence[int] = [m // self._product for m in _moduli]
+        partial_products: Sequence[int] = [
+            m // self._product for m in self._moduli
+        ]
         inverses: Sequence[int] = [
-            pow(p, -1, m) for p, m in zip(partial_products, _moduli)
+            pow(p, -1, m) for p, m in zip(partial_products, self._moduli)
         ]
 
-        self._data: Sequence[Crt._Triple] = [
+        self._data: Sequence[CrtField._Triple] = [
             self._Triple(modulus=m, partial_product=p, inverse=i)
-            for m, p, i in zip(_moduli, partial_products, inverses)
+            for m, p, i in zip(self._moduli, partial_products, inverses)
         ]
+
+    @property
+    def moduli(self) -> Sequence[int]:
+        return self._moduli
+
+    def to_int(self, remainders: Sequence[int]) -> int:
+        """The smallest non-negative integer that produces these remainders
+
+        :param remainders:
+            The remainders corresponding to the *sorted* list of moduli.
+
+        :raises ValueError:
+            if the number of remainders does not match the number moduli
+            for this CRT field.
+        """
+        len_r = len(remainders)
+        len_m = len(self._data)
+        if len_r != len_m:
+            raise ValueError(
+                f"Number of remainders ({len_r}) must match"
+                f"number of moduli {len_m}."
+            )
+        x = 0
+        # TODO: zip this
+        for i in range(len_m):
+            x += (
+                remainders[i]
+                * self._data[i].partial_product
+                * self._data[i].inverse
+            ) % self._product
+        return x
+
+
+class CrtElement:
+    """An element (number) is a CRT Field."""
+
+    def __init__(self, field: CrtField, remainders: Sequence[int]) -> None:
+        """An element of a CRT field from remainders.
+
+        Remainders must be ordered so that the i-th remainder corresponds
+        to the i-th modulus in the field. The moduli in the field
+        are sorted in ascending order.
+        """
+        len_r = len(remainders)
+        len_m = len(field.moduli)
+        if len_r != len_m:
+            raise ValueError(
+                f"Number of remainders ({len_r}) must match"
+                f"number of moduli {len_m}."
+            )
+        self._field = field
+
+        # We will reduce the given remainders
+        self._remainders: Sequence[int] = [
+            r % m for r, m in zip(remainders, self._field.moduli)
+        ]
+
+    @property
+    def field(self) -> CrtField:
+        return self._field
+
+    @property
+    def remainders(self) -> Sequence[int]:
+        return self._remainders
+
+    def __int__(self) -> int:
+        return self._field.to_int(self._remainders)
+
+    def __str__(self) -> str:
+        r_digits = ", ".join((str(r) for r in self._remainders))
+        m_digits = ", ".join((str(m) for m in self._field.moduli))
+
+        return f"({r_digits}) % ({m_digits}))"
